ec870f77dbcedceeb3144a1ba6bfcea346b9ae8c
jesamonen <jesamonen01@gmail.com>
2026-06-14 08:42:49 -0700

    done

--- project/scripts/gallery.js
+++ project/scripts/gallery.js
@@ -16,35 +16,35 @@ const artworks = [
         category: "sculpture",
         region: "Edo State",
         image: "images/benin_bronze.jpg",
-        description: "Royal bronze plaques and sculptures from the Kingdom of Benin."
+        description: "The Benin Bronzes are royal bronze plaques and sculptures created by artists of the Kingdom of Benin in present-day Nigeria.Cast using the lost-wax technique, they depict kings, chiefs, warriors, and important historical events, showcasing the kingdom's rich culture, history, and exceptional craftsmanship."
     },
     {
         title: "Ife Heads",
         category: "sculpture",
         region: "Oyo State",
         image: "images/ife.webp",
-        description: "Highly realistic bronze heads created by the Yoruba civilization."
+        description: "The Ife Bronze Heads are highly realistic bronze and brass sculptures created by the Yoruba civilization in Ile-Ife. They portray kings and other important figures with remarkable detail and are celebrated for their artistic excellence and naturalistic style.."
     },
     {
         title: "Osun Festival Art",
         category: "festival",
         region: "Osun State",
         image: "images/osun.webp",
-        description: "Colorful cultural festival honoring the river goddess Osun."
+        description: "The Osun-Osogbo Festival is a colorful annual cultural festival that honors the river goddess Osun, celebrating Yoruba traditions through sacred rituals, music, dance, and vibrant processions."
     },
     {
         title: "Durbar Celebration",
         category: "festival",
         region: "Kano State",
         image: "images/durbar.webp",
-        description: "Royal horse parade featuring traditional northern Nigerian culture."
+        description: "Royal horse parade featuring traditional northern Nigerian cultureA spectacular royal horse parade celebrating northern Nigerian culture through traditional horsemanship, colorful regalia, music, and ceremonial displays."
     },
     {
         title: "Argungu Festival",
         category: "festival",
         region: "Kebbi State",
         image: "images/argungu.webp",
-        description: "Annual fishing festival attracting thousands of participants."
+        description: "The Argungu Fishing Festival is a traditional Nigerian festival held in Kebbi State that features a large communal fishing competition on the Argungu River, along with cultural dances, music, and celebrations of local heritage."
     },
     {
         title: "Modern Nigerian Painting",
